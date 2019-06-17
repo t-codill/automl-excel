528c5422dcf02f842ec183993a602e9057265e77
Coleman <t-codill@microsoft.com>
2019-06-17 13:20:08 -0700

    added default template

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => res.send('empower every person and every organization on the planet to achieve more'))
+app.use(express.static('dist'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
