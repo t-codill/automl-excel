5378fe8f1114edf7583827b33712ee602b44383a
Coleman <t-codill@microsoft.com>
2019-06-17 15:16:59 -0700

    stuff

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const https = require('https')
+const fs = require('fs')
 const app = express()
 const port = 8080
 
 app.get('/', (req, res) => res.send('empower every person and every organization on the planet to achieve more'))
 app.use(express.static('dist'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+https.createServer({
+    key: fs.readFileSync('local.key'),
+    cert: fs.readFileSync('local.crt')
+}).listen(port, () => console.log(`Example app listening on port ${port}!`))
